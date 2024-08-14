10500df229ce3d10f79dfedcdbabefddb0bd949a
elthickythoy <elthickythoy@gmail.com>
2024-08-14 15:31:39 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'MVRP-Traffic'
+version '1.0.0'
+
+shared_scripts {
+    
+}
+
+server_scripts {
+    
+}
+
+client_scripts {
+    "traffic.lua"
+}
+
+lua54 'yes'
+use_fxv2_oal 'yes'
